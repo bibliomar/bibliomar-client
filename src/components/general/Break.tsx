f2cea2c4fa85068531b80941be1767f5ecd7495b
lamarcke <cassiolamarcksilvafreitas@gmail.com>
2022-09-10 21:26:17 -0300

    reverted break component

--- src/components/general/Break.tsx
+++ src/components/general/Break.tsx
@@ -8,39 +8,22 @@ interface Breaking {
 }
 
 // Talk about code golfing...
+// TODO: refactoring
 export default function Break({ className, mobile, desktop }: Breaking) {
     const size: Size = useWindowSize();
-
-    const onMobile = (): boolean => {
-        return !!(mobile && size.width <= 768);
-    };
-
-    const onDesktop = (): boolean => {
-        return !!(desktop && size.width >= 768);
-    };
-
-    const onBoth = (): boolean => {
-        // Only render on both desktop and mobile if both props are provided, or if not.
-        return !!(desktop && mobile && !desktop && !mobile);
-    };
-
-    const BreakComponent = () => {
-        return (
-            <div className={className ? `break ${className}` : "break"}></div>
-        );
-    };
-
-    const renderBasedOnPlataform = () => {
-        if (onBoth()) {
-            return <BreakComponent />;
-        } else if (onMobile()) {
-            return <BreakComponent />;
-        } else if (onDesktop()) {
-            return <BreakComponent />;
-        } else {
-            return null;
-        }
-    };
-
-    return <>{renderBasedOnPlataform()}</>;
+    return (
+        <>
+            {(!mobile && !desktop) || (mobile && desktop) ? (
+                <div className={`break ${className}`} />
+            ) : mobile && !desktop ? (
+                size.width <= 768 ? (
+                    <div className={`break ${className}`} />
+                ) : null
+            ) : !mobile && desktop ? (
+                size.width >= 768 ? (
+                    <div className={`break ${className}`} />
+                ) : null
+            ) : null}
+        </>
+    );
 }
