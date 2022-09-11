4be2d86ef5b052cf1c5420063468e2ace3c4c777
lamarcke <cassiolamarcksilvafreitas@gmail.com>
2022-09-11 03:56:04 -0300

    Possible fix of reader bug

--- src/components/reader/ReaderLanding.tsx
+++ src/components/reader/ReaderLanding.tsx
@@ -63,6 +63,8 @@ export default function ReaderLanding() {
                     );
                     setBookOnListRetrieved(true);
                 }
+            } else {
+                setBookOnListRetrieved(true);
             }
         });
     }, []);
