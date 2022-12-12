aea8a0e84b03c7efeef9a070dd67999c5a2ac8d8
Matheus do Prado <32469329+theprado@users.noreply.github.com>
2022-12-12 13:15:08 -0300

    updated about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#7663F2',
+        'secondary': '#0E0E52',
+        'third': '#FF8552',
+        'dark': '#343434',
+        'error': '#CE4257',
+        'success': '#519872',
+        'warning': '#FFB100',
+        'modals': '#FAFAFA',
+        'fundo': '#EBECF8',
+      },
+    },
   },
   plugins: [],
 }
