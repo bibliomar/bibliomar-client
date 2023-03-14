1b16fad8dceb422092758cdf9e2d70e375824be9
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2023-03-14 15:35:25 -0300

    - Badge text for main screen

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -403,7 +403,7 @@ function Search() {
             </Helmet>
             <div className="row ">
                 <div className="col mt-3">
-                    <Navbar activeItem="home" />
+                    <Navbar activeItem="home" badgeText="SEARCH" />
                 </div>
             </div>
 
